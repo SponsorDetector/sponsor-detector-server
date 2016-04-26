2bebb6cd5fa6714e04b71ba1f17988344f696199
Lenjy <piednoir.jocelyn@gmail.com>
2016-04-26 23:33:56 +0200

    Fix

---
+++ api/controllers/elasticsearch.js
@@ -0,0 +1,172 @@
+var elasticsearch = require('elasticsearch');
+
+var elasticClient = new elasticsearch.Client({
+    host: 'localhost:9300',
+    log: 'info'
+});
+
+var configuration = "configurationIndex";
+var sponsoredContent = "sponsoredContentIndex";
+
+/**
+* Delete an existing configuration index
+*/
+function deleteConfigurationIndex() {
+    return elasticClient.indices.delete({
+        index: configuration
+    });
+}
+exports.deleteConfigurationIndex = deleteConfigurationIndex;
+
+/**
+* create the configuration index
+*/
+function initConfigurationIndex() {
+    return elasticClient.indices.create({
+        index: configuration
+    });
+}
+exports.initConfigurationIndex = initConfigurationIndex;
+
+/**
+* check if the configuration index exists
+*/
+function indexConfigurationExists() {
+    return elasticClient.indices.exists({
+        index: configuration
+    });
+}
+exports.indexConfigurationExists = indexConfigurationExists;
+
+/**
+* create the sponsoredContent index
+*/
+function initSponsoredContentIndex() {
+    return elasticClient.indices.create({
+        index: sponsoredContent
+    });
+}
+exports.initSponsoredContentIndex = initSponsoredContentIndex;
+
+/**
+* check if the sponsoredContent index exists
+*/
+function indexSponsoredContentExists() {
+    return elasticClient.indices.exists({
+        index: sponsoredContent
+    });
+}
+exports.indexSponsoredContentExists = indexSponsoredContentExists;
+
+/**
+* Delete an existing sponsoredContent index
+*/
+function deleteSponsoredContentIndex() {
+    return elasticClient.indices.delete({
+        index: sponsoredContent
+    });
+}
+exports.deleteSponsoredContentIndex = deleteSponsoredContentIndex;
+
+function initMapping() {
+    return elasticClient.indices.putMapping(
+      {
+        index: configuration,
+        type: "configuration",
+        body: 
+        {
+          dynamic_templates : [
+              {
+                string_fields: {
+                  match: "*",
+                  match_mapping_type: "string",
+                  mapping: {
+                    index: "not_analyzed",
+                    omit_norms: true,
+                    type: "string"
+                  }
+                }
+              }
+            ],
+        properties: {
+          pushed_at: {
+            type: "date",
+            format : "yyyy-MM-dd HH:mm"
+          }
+        }
+      }
+    },
+  {
+    index: sponsoredContent,
+    type: "sponsoredContent",
+    body:{
+      dynamic_templates: [
+          {
+            string_fields: {
+              match: "*",
+              match_mapping_type: "string",
+              mapping: {
+                index: "not_analyzed",
+                omit_norms: true,
+                type: "string"
+              }
+            }
+          }
+        ],
+    properties: {
+      pushed_at: {
+        type: "date",
+        format : "yyyy-MM-dd HH:mm"
+      }
+    }
+  }
+});
+}
+exports.initMapping = initMapping;
+
+function addConfiguration(configuration) {
+    return elasticClient.index({
+        index: configuration,
+        type: "configuration",
+        body: {
+            domain: configuration.domain,
+            name: configuration.name,
+            author: {
+                extractor: {
+                  name : configuration.author.extractor.name,
+                  params : configuration.author.extractor.params.split(",")
+                }
+            },
+            sponsor: {
+                detector: {
+                  name : configuration.sponsor.detector.name,
+                  params : configuration.sponsor.detector.params.split(",")
+                },
+                extractor: {
+                  name : configuration.sponsor.extractor.name,
+                  params : configuration.sponsor.extractor.params.split(",")
+                }
+            },
+            status : configuration.status
+        }
+    });
+}
+exports.addConfiguration = addConfiguration;
+
+
+function addSponsoredContent(sponsoredContent) {
+    return elasticClient.index({
+        index: sponsoredContent,
+        type: "sponsoredContent",
+        body: {
+            id: sponsoredContent.id,
+            author: sponsoredContent.author,
+            sponsor: sponsoredContent.sponsor,
+            domain: sponsoredContent.domain,
+            title: sponsoredContent.title,
+            link: sponsoredContent.link
+            }
+        }
+    );
+}
+exports.addSponsoredContent = addSponsoredContent;
