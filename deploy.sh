35ae5ec24e2dc98ede61625e9889ee19111cbce9
ogdabou <ogdabou@protonmail.com>
2016-05-18 15:21:59 +0200

    trying to deploy

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd dist;
+pwd;
+git init;
+git config --global user.name "travis"
+git config --global user.email "travis@github.com"
+git remote add origin $DEV_DEPLOY_REPO;
+git add .;
+git commit -m "Build $TRAVIS_BUILD_NUMBER";
+git push deploy master;
+
