3fd712fb6f75ecb5a080e4d254ec8682794df7f1
ogdabou <ogdabou@protonmail.com>
2016-05-18 16:06:54 +0200

    Correction

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ pwd;
 git init;
 git config --global user.name "travis"
 git config --global user.email "travis@github.com"
-git remote add origin $DEV_DEPLOY_REPO;
+git remote add deploy $DEV_DEPLOY_REPO;
 git add .;
 git commit -m "Build $TRAVIS_BUILD_NUMBER";
 git push deploy master;
