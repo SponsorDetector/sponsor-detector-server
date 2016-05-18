84f49563f6dd2f616cff4bf60fccdc08a7f0d909
ogdabou <ogdabou@protonmail.com>
2016-05-18 15:44:16 +0200

    using key

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+eval "$(ssh-agent -s)"
+chmod 600 key
+ssh-add key
 cd dist;
 pwd;
 git init;
@@ -9,4 +11,3 @@ git remote add origin $DEV_DEPLOY_REPO;
 git add .;
 git commit -m "Build $TRAVIS_BUILD_NUMBER";
 git push deploy master;
-
